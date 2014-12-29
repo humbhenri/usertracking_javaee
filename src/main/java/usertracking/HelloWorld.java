07d10fd849e3e951858fc425aa5a0cfd1ee8ecce
Humberto.Pinheiro <humbhenri@gmail.com>
2014-12-29 11:43:52 -0200

    faces hello world

---
+++ src/main/java/usertracking/HelloWorld.java
@@ -0,0 +1,11 @@
+package usertracking;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloWorld {
+
+    public String getMessage() {
+        return "Hello World from JEE!";
+    }
+}
